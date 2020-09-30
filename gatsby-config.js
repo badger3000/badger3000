bd90917c50de0bf28ece8605535c5fd5939b0dce
Kyle Ross <kyle@badger3000.com>
2020-09-30 16:06:23 -0700

    fixed PWA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,3 @@
-const path = require(`path`)
-
 module.exports = {
   siteMetadata: {
     title: 'Kyle Ross - Web Developer',
@@ -8,29 +6,29 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-
     {
-      resolve: `gatsby-plugin-manifest`,
-
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: './',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display:  'minimal-ui',
-        icon: "src/assets/favicons/images/apple-icon.png", // This path is relative to the root of the site.
-      },
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `assets/images/`),
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Badger3000',
+        short_name: 'Badger3000',
+        start_url: '/',
+        background_color: '#49bf9d',
+        theme_color: '#49bf9d',
+        display: 'standalone',
+        icon: 'src/assets/images/website-icon.png',
+        orientation: 'portrait', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-netlify',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     'gatsby-plugin-offline',
   ],
 }
