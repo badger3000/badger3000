40c147623576b12d62e21d8fb725f6fa2451ae22
Kyle Ross <kyle@badger3000.com>
2020-09-30 14:13:27 -0700

    config updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,50 +13,13 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
 
       options: {
-        name: 'Badger3000',
-        short_name: 'Badger3000',
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
         start_url: './',
         background_color: '#663399',
         theme_color: '#663399',
-        display: `standalone`,
-        icon: [
-          {
-            src: 'src/assets/images/favicons/android-icon-36x36.png',
-            sizes: '36x36',
-            type: 'image/png',
-            density: '0.75',
-          },
-          {
-            src: 'src/assets/images/favicons/android-icon-48x48.png',
-            sizes: '48x48',
-            type: 'image/png',
-            density: '1.0',
-          },
-          {
-            src: 'src/assets/images/favicons/android-icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-            density: '1.5',
-          },
-          {
-            src: 'src/assets/images/favicons/android-icon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-            density: '2.0',
-          },
-          {
-            src: 'src/assets/images/favicons/android-icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-            density: '3.0',
-          },
-          {
-            src: 'src/assets/images/favicons/android-icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            density: '4.0',
-          },
-        ], // This path is relative to the root of the site.
+        display:  'minimal-ui',
+        icon: "src/assets/favicons/images/apple-icon.png", // This path is relative to the root of the site.
       },
       resolve: `gatsby-source-filesystem`,
       options: {
