ce9f4d0ef22e05c318945e54bbe9b73076af5977
Kyle Ross <kyle@badger3000.com>
2021-11-05 17:05:02 -0700

    updated location and bg image

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ const Header = ({ className }) => {
           <strong>Kyle Ross</strong>
         </h1>
         <p style={{ marginBottom: 0, fontSize: '1.5em' }}>Web Developer</p>
-        <p style={{ marginBottom: 0, fontSize: '1.5em' }}>Bay Area, CA</p>
+        <p style={{ marginBottom: 0, fontSize: '1.5em' }}>Prescott, AZ</p>
       </div>
       <Footer />
     </BackgroundImage>
