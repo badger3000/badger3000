03545f2861a79d6b61adb44b47c0c11a7fde5c54
Kyle Ross <kyle@badger3000.com>
2023-01-23 15:07:41 -0700

    added lang to HTML, removed the extra staticQuery that was breaking develop build

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
-
-import { convertToBgImage } from 'gbimage-bridge'
-import BackgroundImage from 'gatsby-background-image'
 // Note: You can change "images" to whatever you'd like.
 
 export default function Image(props) {
@@ -41,49 +38,6 @@ export default function Image(props) {
             alt={props.alt}
             style={{ position: 'revert' }}
           />
-        )
-      }}
-    />
-  )
-}
-export const BackgroundImages = (props, { children }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        {
-          images: allFile {
-            edges {
-              node {
-                publicURL
-                relativePath
-                name
-                childImageSharp {
-                  gatsbyImageData(width: 600, layout: CONSTRAINED)
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={(data) => {
-        const image = data.images.edges.find((n) => {
-          return n.node.relativePath.includes(props.filename)
-        })
-        if (!image) {
-          return null
-        }
-
-        // Use like this:
-        const bgImage = convertToBgImage(image)
-        return (
-          <BackgroundImage
-            Tag="section"
-            // Spread bgImage into BackgroundImage:
-            {...bgImage}
-            preserveStackingContext
-          >
-            {children}
-          </BackgroundImage>
         )
       }}
     />
