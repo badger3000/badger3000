21eb3d01ded6c560401363fe890315db450dc6d2
Kyle Ross <kyle@badger3000.com>
2023-01-23 20:44:31 -0700

    updated query to use gatsby static hook

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,45 +1,39 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 // Note: You can change "images" to whatever you'd like.
 
 export default function Image(props) {
-  return (
-    <StaticQuery
-      query={graphql`
-        {
-          images: allFile {
-            edges {
-              node {
-                publicURL
-                relativePath
-                name
-                childImageSharp {
-                  gatsbyImageData(width: 600, layout: CONSTRAINED)
-                }
-              }
+  const data = useStaticQuery(graphql`
+    query Images {
+      allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+        edges {
+          node {
+            name
+            childImageSharp {
+              gatsbyImageData(width: 600, layout: CONSTRAINED)
             }
+            publicURL
+            relativePath
           }
         }
-      `}
-      render={(data) => {
-        const image = data.images.edges.find((n) => {
-          return n.node.relativePath.includes(props.filename)
-        })
-        if (!image) {
-          return null
-        }
+      }
+    }
+  `)
+  const image = data.allFile.edges.find((n) => {
+    return n.node.relativePath.includes(props.filename)
+  })
+  if (!image) {
+    return null
+  }
 
-        return (
-          <GatsbyImage
-            image={image.node.childImageSharp.gatsbyImageData}
-            loading="eager"
-            fit="cover"
-            alt={props.alt}
-            style={{ position: 'revert' }}
-          />
-        )
-      }}
+  return (
+    <GatsbyImage
+      image={image.node.childImageSharp.gatsbyImageData}
+      loading="eager"
+      fit="cover"
+      alt={props.alt}
+      style={{ position: 'revert' }}
     />
   )
 }
