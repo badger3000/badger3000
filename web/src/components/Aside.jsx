49a226ab613c15ce7f859ea0de59546e660a6c61
Kyle Ross <kyle@badger3000.com>
2023-01-26 15:37:09 -0700

    basic layout with tailwinds

--- web/src/components/Aside.jsx
+++ web/src/components/Aside.jsx
@@ -4,7 +4,7 @@ import Footer from './Footer'
 
 const Aside = () => {
   return (
-    <aside id="header" className="basis-3/12 flex-col">
+    <aside id="header" className="basis-3/12 flex-col md:mt-2 md:mr-2">
       <InnerBlock />
       <Footer />
     </aside>
