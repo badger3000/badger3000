50c6ac5fa9ec59fff503e5a7ebfd04b3703bf0fd
Kyle Ross <kyle@badger3000.com>
2023-01-28 21:25:57 -0700

    background css added

--- web/src/components/layout.jsx
+++ web/src/components/layout.jsx
@@ -3,16 +3,9 @@ import React from 'react'
 import Aside from './Aside/Aside'
 
 const Layout = ({ children }) => (
-  <div
-    style={{
-      backgroundImage:
-        'linear-gradient(to left bottom, #000000, #141414, #212021, #2e2d2e, #3b3b3c, #414142, #474849, #4d4e4f, #4d4e4f, #4e4e4f, #4e4e4e, #4e4e4e)',
-    }}
-  >
-    <div className="container mx-auto flex min-h-screen flex-col scroll-smooth md:mr-0 md:flex-row">
-      <Aside />
-      {children}
-    </div>
+  <div className="container mx-auto flex min-h-screen flex-col scroll-smooth md:mr-0 md:flex-row">
+    <Aside />
+    {children}
   </div>
 )
 
