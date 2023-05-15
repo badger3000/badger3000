a9ca9250843c6b1a76503dd9b60416bd76073196
Kyle Ross <kyle@badger3000.com>
2023-05-15 10:25:00 -0700

    images
    - support for images
    - removed files I do not what to track

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-});
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
+})
