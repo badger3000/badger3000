241ac9c6bb6cd5d0fe4d36d3060906c2112772a0
Kyle Ross <kyle@badger3000.com>
2023-05-21 01:03:14 -0700

    moved images into src dir, added mobile nav funcinality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 import react from "@astrojs/react";
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), react()]
+  }), react(), alpinejs()]
 });
