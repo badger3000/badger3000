ccebc70c2ed66f71d79ff6da1f0626f49b1ba183
Kyle Ross <kyle@badger3000.com>
2023-06-02 09:16:14 -0700

    WIP
    - added sitemap
    - updated pie chart

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,18 +2,20 @@ import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-
 import alpinejs from '@astrojs/alpinejs'
 
-// https://astro.build/config
 import sanity from 'astro-sanity'
 // Sanity project info
+import sitemap from '@astrojs/sitemap'
 const SANITY_PROJECT_ID = import.meta.env.VITE_SANITY_PROJECT_ID
 const SANITY_DATASET = import.meta.env.VITE_SANITY_DATASET
 const SANITY_TOKEN = import.meta.env.VITE_SANITY_TOKEN
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://badger3000.com/',
   integrations: [
     tailwind(),
     image({
@@ -28,5 +30,6 @@ export default defineConfig({
       apiVersion: '2021-03-25',
       useCdn: false,
     }),
+    sitemap(),
   ],
 })
