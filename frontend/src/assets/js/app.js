4492af82edbe3d0ed2ef647d44f29086fb75276a
Kyle Ross <kyle@badger3000.com>
2023-10-01 10:47:46 -0700

    updated avatar, removed swap and used astro page transitions. updated packages

--- frontend/src/assets/js/app.js
+++ frontend/src/assets/js/app.js
@@ -1,12 +1,3 @@
-import Swup from 'swup'
-import SwupA11yPlugin from '@swup/a11y-plugin'
-import SwupHeadPlugin from '@swup/head-plugin'
-import SwupSlideTheme from '@swup/slide-theme'
-
-//page transitions
-const swup = new Swup({
-  plugins: [new SwupA11yPlugin(), new SwupHeadPlugin(), new SwupSlideTheme()],
-})
 //mobile menu
 const mobileMenu = () => {
   //hamburger menu toggle
