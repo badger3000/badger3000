0a3918247874d5d925cd9bac98f4e41a4bd5adb7
Kyle Ross <kyle@badger3000.com>
2025-05-05 16:57:26 -0700

    updated remote patterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.sanity.io"], // If you're using Sanity for images
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/**",
+      },
+    ],
   },
   env: {
     SITE_URL: process.env.SITE_URL || "https://www.badger3000.com/",
